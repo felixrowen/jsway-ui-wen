536f63841019a0b1b4ec0e6595c3fa90705bd01a
felixrowen <felixroo98@gmail.com>
2019-09-22 20:55:23 +0700

    add new question (anagram difference)

--- client/src/components/questions/ListOfQuestions.js
+++ client/src/components/questions/ListOfQuestions.js
@@ -10,6 +10,12 @@ export const ListOfQuestions = [
     example: "Fibonacci Number : 1 1 2 3 5 8 13 ..... || F(4) = F(3) + F(2) = 2 + 1 = 3.",
     input: "4",
     output: "3"
+  },
+  {
+    title: "Anagram Difference",
+    example: "n = 5 , ( a jk abb mn abc ) ( bb kj bbc op def ), a and bb return -1, jk and kj return 0 because they have the same char, abb to bbc return 1 because a -> c, mn to op return 2 because 2 different char, abc to def return 3 because all the char is different.",
+    input: "[a, jk, abb, mn, abc] and [bb, kj, bbc, op, def]",
+    output: "-1,0,1,2,3"
   }
 
 ]
